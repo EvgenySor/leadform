92760323a3b9cb904f1eeb63a4a7481b69e7c7cf
Evgeny Sorochuk <sorgena2000@gmail.com>
2021-09-18 20:11:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
-if (isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
-  $secret = '6LesDnccAAAAAAaeCaVG8MVfAZ5WegYe9vwB9Mmi';
-  $ip = $_SERVER['REMOTE_ADDR'];
-  $response = $_POST['g-recaptcha-response'];
-  $rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$ip");
-  $arr = json_decode($rsp, TRUE);
-  if ($arr['success']) {
-    ...
-  }
-}
-?>
 
 
 <!DOCTYPE html>
