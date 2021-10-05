c502756917bbbd5e23d15b6cab31f78aba6bd125
Evgeny Sorochuk <sorgena2000@gmail.com>
2021-10-05 23:14:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 print(pg_connection_string_from_database_url());
 
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT name, productcode FROM salesforce.product2 WHERE schemaname='salesforce'");
+$result = pg_query($pg_conn, "SELECT name, productcode FROM salesforce.product2");
 
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+  /*print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }*/
 }
 print "\n";*/
 ?>
