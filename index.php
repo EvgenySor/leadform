0514a0d431ec1ae8664d40e991e38acbfa455be2
Evgeny Sorochuk <sorgena2000@gmail.com>
2021-10-06 00:00:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,6 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 # Now let's use the connection for something silly just to prove it works:
 $result = pg_query($pg_conn, "SELECT name, productcode FROM salesforce.product2");
-
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  //print "Tables in your database:\n";
-  //while ($row = pg_fetch_row($result)) { print("- $row[0], $row[1]\n"); }
-}
 ?>
 
 
@@ -75,20 +68,6 @@ if (!pg_num_rows($result)) {
 							<?php while ($row = pg_fetch_row($result)) : ?>
 								<option value="<?=$row[1];?>"><?=$row[0];?></option>
 							<?php endwhile ;?>
-
-							<?/*php
-								while ($row = pg_fetch_row($result)) { 
-									print("<option>" . "$row[0]" . "</option>"); 
-								}	
-							*/?>
-
-
-							<?/*php
-								while ($row = pg_fetch_row($result)) { 
-									print("- $row[0], $row[1]\n"); 
-								}
-							*/?>
-							
 						</select>
 						<label>Product:</label>
 					</div>
