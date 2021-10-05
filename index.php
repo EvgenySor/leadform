313368050b608ffd55d50103923793577baff54f
Evgeny Sorochuk <sorgena2000@gmail.com>
2021-10-05 23:26:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
   print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+  while ($row = pg_fetch_row($result)) { print("- $row[0].name, $row[0].productcode\n"); }
 }
 print "\n";
 ?>
